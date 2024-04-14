c3a3dcd5fcfbcdffc511a6e192f5f7a614e801e5
harshamin <hamin04@students.cs.ubc.ca>
2024-04-14 03:20:35 -0700

    updated express json parser

--- Back-end/3.5 Secrets Project/index.js
+++ Back-end/3.5 Secrets Project/index.js
@@ -4,26 +4,21 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 var pass = "";
 
-app.use(bodyParser.urlencoded({ extended: true}));
-
-app.use((req,res,next) => {
-    pass = req.body["password"];
-    next();
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/check", (req,res) => {
-    if (pass === "ILoveProgramming") {
+    if (req.body.password === "ILoveProgramming") {
         res.sendFile(__dirname + "/public/secret.html");
     } else {
         res.redirect("/");
